c11e1cc65750fbe1f6133295ab19e0d5512aa847
Aykut Cengiz <49622855+aykutceng@users.noreply.github.com>
2020-07-22 18:10:04 +0300

    Update and rename h-2_w-1_oraninda_beyaz_arkaplanli_resim_duzenleme.py to ResizeImagesByFillingWithWhiteBackground_Ratio_1_to_2.py

--- ResizeImagesByFillingWithWhiteBackground_Ratio_1_to_2.py
+++ ResizeImagesByFillingWithWhiteBackground_Ratio_1_to_2.py
@@ -2,49 +2,27 @@ import cv2
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename, askdirectory
 import re
-# load the image and show it
-# image = cv2.imread("650x344-deneme-nedir-ve-nasil-yazilir-1540990804971.jpg")
-# print(image.shape)
-# cv2.imshow("original", image)
-# cv2.waitKey(0)
-
-# cropped = image[0:800, 0:650]
-# cv2.imshow("resize", cropped)
-# cv2.waitKey(0)
-
 
 hi = None
 wi = None
 
 def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
     dim = None
     (h, w) = image.shape[:2]
 
-    # if both the width and height are None, then return the
-    # original image
     if width is None and height is None:
         return image
 
-    # check to see if the width is None
     if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
         r = height / float(h)
         dim = (int(w * r), height)
 
-    # otherwise, the height is None
     else:
-        # calculate the ratio of the width and construct the
-        # dimensions
         r = width / float(w)
         dim = (width, int(h * r))
 
-    # resize the image
     resized = cv2.resize(image, dim, interpolation = inter)
 
-    # return the resized image
     return resized
 
 
@@ -71,11 +49,6 @@ def RS(ImageFilePath,name):
         background = background.convert('RGB')
         background.save(ImageFilePath)
         print("Image has been resized !")
-        #out = cv2.imread(ImageFilePath,0)
-        #out = image_resize(out, width = wi, height = hi, inter = cv2.INTER_AREA)
-        #cv2.imwrite(ImageFilePath,out)
-        #cv2.imshow("resize", out)
-        #cv2.waitKey(0)
     
     elif(width < height and height < (2 * width)):
         bigside = width
@@ -88,11 +61,6 @@ def RS(ImageFilePath,name):
         background = background.convert('RGB')
         background.save(ImageFilePath)
         print("Image has been resized !")
-        #out = cv2.imread(ImageFilePath,0)
-        #out = image_resize(out, width = wi, height = hi, inter = cv2.INTER_AREA)
-        #cv2.imwrite(ImageFilePath,out)
-        #cv2.imshow("resize", out)
-        #cv2.waitKey(0)
     
     elif(width < height and height > (2*width)):
         bigside = int(height / 2)
@@ -105,11 +73,6 @@ def RS(ImageFilePath,name):
         background = background.convert('RGB')
         background.save(ImageFilePath,0)
         print("Image has been resized !")
-        #out = cv2.imread(ImageFilePath)
-        #out = image_resize(out, width = wi, height = hi, inter = cv2.INTER_AREA)
-        #cv2.imwrite(ImageFilePath,out)
-        #cv2.imshow("resize", out)
-        #cv2.waitKey(0)
 
     else:
         print("Image is already a proper, it has not been resized !")
@@ -121,17 +84,10 @@ def inp():
         
         hi = None
         wi = None
-        hw = str(input("Yükseklik mi yoksa genişlik üzerinden mi ölçeklendirme yapmak istersiniz (1 / 2)? (Yükseklik için 1, genişlik için 2 giriniz)"))
-        #dm = int(input("Ölçek değerini giriniz!"))
-        #if re.match(r'[1]{1}',dm) and not re.match(r'([1]{1})([0-9a-zA-Z-_?./\\\'"*+|%&$#£>é<]{1,})',dm):
-        
-        #if hw == "1":
-        #    hi = dm
-        #if hw == "2":
-        #    wi = dm
-        #print(dm,hw,hi,wi)
+        hw = str(input("Which property do you want to use to resize images? 1 for height, 2 for width"))
+
     except:
-        print("Lütfen geçerli bir parametre giriniz!!!")
+        print("Please enter a valid parameter!!!")
         return inp()
 
 
@@ -149,7 +105,7 @@ def scan(path):
 
 
 Tk().withdraw()
-path = askdirectory(title = "İsimleri düzenlenecek resimlerin bulunduğu ana klasörü seçiniz!")
+path = askdirectory(title = "Please choose parent folder which contains sub folders and images!")
 #print(path)
 
 #inp()
